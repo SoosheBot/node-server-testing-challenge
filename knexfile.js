8f414dbbc7e501f7d3be6ed2bd4ac618584b7602
sooshe <sooshe@sooshebot.com>
2020-02-27 20:42:01 -0600

    update students router

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: "postgresql://localhost/school",
+    connection: "postgresql://localhost/cohorts",
     migrations: {
       directory: "./data/migrations",
     },
