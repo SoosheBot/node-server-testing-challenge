6d6a4ca955ed316e122bbe568cce81186f3c18fa
sooshe <sooshe@sooshebot.com>
2020-02-27 21:36:20 -0600

    wrote tests for student router -- MVP REACHED

--- students/router.js
+++ students/router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.json({ router: "students" });
+  res.status(201).json({ router});
 });
 
 module.exports = router;
--- students/students.test.js
+++ students/students.test.js
@@ -1,15 +1,26 @@
 const request = require("supertest");
 
 const server = require("../api/server");
-
 describe("students router", function() {
-    it("should run the tests", function() {
-      expect(true).toBe(true);
+  it("should run the tests", function() {
+    expect(true).toBe(true);
+  });
+
+  describe("GET /api/cohorts", function() {
+    it("should return 201 OK", function() {
+      return request(server)
+        .get("/api/students")
+        .then(res => {
+          expect(res.status).toBe(201);
+        });
     });
 
-    it("should return students as the router value (async version)", async function() {
-         const res = await request(server).get("/api/students");
-
-         expect(res.body.router).toBe("students");
-     });
+    it("should return JSON formatted body", function() {
+      return request(server)
+        .get("/api/students")
+        .then(res => {
+          expect(res.type).toMatch(/json/);
+        });
+    });
+  });
 });
